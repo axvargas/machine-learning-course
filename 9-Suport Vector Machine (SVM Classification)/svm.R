da6bda31764504f58f89c94c80a5a70ed09a0caa
Andres X. Vargas <axvargas@fiec.espol.edu.ec>
2021-03-24 16:26:43 -0500

    SVM classification

--- 9-Suport Vector Machine (SVM Classification)/svm.R
+++ 9-Suport Vector Machine (SVM Classification)/svm.R
@@ -1,3 +1,5 @@
+#Maquinas de soporte vectorial (SVM)
+
 # Plantilla de clasificacion
 
 #import dataset
@@ -19,15 +21,16 @@ training_set[,1:2] = scale(training_set[,1:2])
 testing_set[,1:2] = scale(testing_set[,1:2]) 
 
 #Ajustar el clasificador de regresion logistica con el conjunto de entrenamiento
-# --COde goes here
-
-
+library(e1071)
+classifier = svm(formula = Purchased ~ .,
+                 data = training_set,
+                 type = 'C-classification',
+                 kernel = 'linear')
 
 #Prediccion deos resultados del conjunto de testing
-#prob_pred = predict(classifier, type = "response", newdata = testing_set[,-3])#solo para logistica
-#y_pred = ifelse(prob_pred > 0.5, 1, 0)
-#para las demas
 y_pred = predict(classifier, newdata = testing_set[,-3])
+
+#y_pred = ifelse(prob_pred > 0.5, 1, 0) #ya no hace falta
 
 #Crear la matriz de confusiones
 cm = table(testing_set[,3], y_pred)
@@ -39,13 +42,9 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-#solo para logistica
-#prob_set = predict(classifier, type = 'response', newdata = grid_set)
-#y_grid = ifelse(prob_set > 0.5, 1, 0)
-##Para las demas
-#y_grid = predict(classifier, newdata = grid_set)
+y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Logistic Regression (Training set)',
+     main = 'SVM classification (Training set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -58,13 +57,9 @@ X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
 X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-#solo para logistica
-#prob_set = predict(classifier, type = 'response', newdata = grid_set)
-#y_grid = ifelse(prob_set > 0.5, 1, 0)
-##Para las demas
-#y_grid = predict(classifier, newdata = grid_set)
+y_grid = predict(classifier, newdata = grid_set)
 plot(set[, -3],
-     main = 'Logistic Regression (Test set)',
+     main = 'SVM classification (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
